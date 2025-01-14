e0046a2e002e82cfbbe69a35c209a8815413f85d
mac <mac@macs-MacBook-Pro.local>
2025-01-14 18:04:29 +0900

    Feat: level4 1차 구현📰

---
+++ src/main/java/com/example/kiosk/level4/Kiosk.java
@@ -0,0 +1,103 @@
+package com.example.kiosk.level4;
+
+import java.util.*;
+
+public class Kiosk {
+
+    private final List<Menu> menuList;
+
+    public Kiosk(List<Menu> menuList) {
+        this.menuList = menuList;
+    }
+
+    public void start() {
+
+        // Scanner 선언
+        Scanner sc = new Scanner(System.in);
+
+        // 반복문 시작
+        while (true) {
+            System.out.println();
+
+            // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
+            System.out.println("[ MAIN MENU ]");
+            for (Menu menu : menuList) {
+                System.out.println((menuList.indexOf(menu) + 1) + ". " + menu.showCategory());
+            }
+            System.out.println("0. 종료      | 종료");
+
+            try {
+                // 숫자를 입력 받기
+                System.out.println("inputFirstNumber");
+                int inputFirstNumber = sc.nextInt();
+
+                if (inputFirstNumber == 0) {
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                }
+
+                // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+                // List<Menu>에 인덱스로 접근하면 Menu만 추출할 수 있겠죠?
+                Menu menu = menuList.get(inputFirstNumber - 1);
+                List<MenuItem> menuItems = menuList.get(inputFirstNumber - 1).getMenuItems();
+
+                System.out.println("[ " + menuList.get(inputFirstNumber - 1).showCategory().toUpperCase() + " MENU ]");
+
+                // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
+                menu.showMenuItem();
+
+                System.out.println("0. 뒤로가기");
+
+                // 숫자 입력 받기
+                // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
+                // menu.getMenuItems().get(i); 같은 형식으로 하나씩 들어가서 얻어와야 합니다.
+                // 숫자를 입력 받기
+                System.out.println("inputSecondNumber");
+                int inputSecondNumber = sc.nextInt();
+
+                MenuItem menuItem;
+                switch (inputSecondNumber) {
+                    case 0:
+                        System.out.println("뒤로 갑니다.");
+                        break;
+                    case 1:
+                         menuItem = menuItems.get(0);
+                         System.out.printf("1. %s | W %.1f | %s%n", menuItem.getName(),menuItem.getPrice(),menuItem.getInfo());
+//                        System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                        break;
+                    case 2:
+                        menuItem = menuItems.get(1);
+                        System.out.printf("2. %s | W %.1f | %s%n", menuItem.getName(),menuItem.getPrice(),menuItem.getInfo());
+//                        System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                        break;
+                    case 3:
+                        menuItem = menuItems.get(2);
+                        System.out.printf("3. %s | W %.1f | %s%n", menuItem.getName(),menuItem.getPrice(),menuItem.getInfo());
+//                        System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                        break;
+                    case 4:
+                        menuItem = menuItems.get(3);
+                        System.out.printf("4. %s | W %.1f | %s%n", menuItem.getName(),menuItem.getPrice(),menuItem.getInfo());
+//                        System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                        break;
+                    default:
+                        throw new IllegalArgumentException("보기 중에 없는 번호이거나 숫자가 아닙니다. 다시 입력해주세요.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("숫자가 아닌 값입니다. 다시 입력하세요.");
+                sc.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println("보기 중에 없는 번호입니다. 다시 입력해주세요.");
+                sc.nextLine();
+            } catch (NullPointerException e) {
+                System.out.println("상세 내역이 없는 메뉴입니다. 다시 입력해주세요.");
+                sc.nextLine();
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("보기 중에 없는 번호입니다. 다시 입력해주세요.");
+                sc.nextLine();
+            }
+        }
+    }
+}
+
+
---
+++ src/main/java/com/example/kiosk/level4/Menu.java
@@ -0,0 +1,36 @@
+package com.example.kiosk.level4;
+
+import java.util.*;
+
+public class Menu {
+
+    // MenuItem 클래스를 List로 관리
+    // List 선언 및 초기화
+    private List<MenuItem> menuItems;
+    private String categroy;
+
+    public Menu(String categroy) {
+        this.categroy = categroy;
+    }
+
+    public String showCategory(){
+        return categroy;
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    // List에 들어있는 MenuItem을 순차적으로 보여주는 함수
+    public void showMenuItem(){
+        for (MenuItem menuItem : menuItems) {
+            int index = menuItems.indexOf(menuItem)+1;
+            System.out.printf("%d. %s | W %.1f | %s%n",index, menuItem.getName(),menuItem.getPrice(),menuItem.getInfo());
+        }
+    }
+
+    // List를 리턴하는 함수
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
