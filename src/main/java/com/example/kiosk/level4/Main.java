0757e1a2b144cd2d491fffa8254b22ebddd2ecb0
mac <mac@macs-MacBook-Pro.local>
2025-01-16 09:34:48 +0900

    Fix: level4,5 테스트 데이터 제거 📝

--- src/main/java/com/example/kiosk/level4/Main.java
+++ src/main/java/com/example/kiosk/level4/Main.java
@@ -19,12 +19,6 @@ public class Main {
         MenuItem Hamburger = new MenuItem("Hamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거");
         burgers.setMenuItems(List.of(shackBurger,SmokeShack,Cheeseburger,Hamburger));
 
-        MenuItem drinks1 = new MenuItem("DShackBurger",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem drinks2 = new MenuItem("DSmokeShack",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem drinks3 = new MenuItem("DCheeseburger",6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem drinks4 = new MenuItem("DHamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거");
-        drinks.setMenuItems(List.of(drinks1,drinks2,drinks3,drinks4));
-
         // Kiosk 객체 생성
         Kiosk kiosk = new Kiosk(menuList);
 
